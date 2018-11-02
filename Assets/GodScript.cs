25aecc1406424cbcd112a0d448daefc13dedabae
Reni M <aqua1111111@gmail.com>
2018-11-02 17:01:49 +0000

    Added explosions. They don't work as expected....

--- Assets/GodScript.cs
+++ Assets/GodScript.cs
@@ -13,6 +13,7 @@ public class GodScript : MonoBehaviour {
 	private float timeElapsed = 0.0f;
 	private Canvas canvas;
 	public Text timeText;
+	
 
 	bool AllFound() {
 		List<bool> allFound = new List<bool>{cpu.detected, motherBoard.detected, ram.detected, fan.detected };
@@ -25,12 +26,24 @@ public class GodScript : MonoBehaviour {
 
 	}
 
+	void Start()
+    {
+    }
+
+ 	void Explode( ParticleSystem exp ) {
+        exp.Play();
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
 		if (!AllFound()){
 			timeElapsed += Time.deltaTime;
+		} else {
+		 	 ParticleSystem fireworksGameObject;
+			 ParticleSystem explosionsGameObject;
 		}
+
         TimeSpan time = TimeSpan.FromSeconds(Mathf.RoundToInt(timeElapsed));
         timeText.text = ("TIME: " + time.ToString());
         Debug.Log( "TIME: " + time.ToString()); // 00:03:48
